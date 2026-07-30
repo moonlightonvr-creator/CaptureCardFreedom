0b605a2189b0089728e247a8623ad541a33ca4cf
moonlightonvr-creator <moonlightonvr@gmail.com>
2026-07-30 23:46:59 +0100

    Update MainActivity.java

--- app/src/main/java/com/capturecardfreedom/MainActivity.java
+++ app/src/main/java/com/capturecardfreedom/MainActivity.java
@@ -1,1 +1,28 @@
+package com.capturecardfreedom;
 
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends Activity {
+
+    TextView statusText;
+    Button recordButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+
+        statusText = findViewById(R.id.statusText);
+        recordButton = findViewById(R.id.recordButton);
+
+        statusText.setText("USB Status: Ready");
+
+        recordButton.setOnClickListener(v -> {
+            statusText.setText("Recording: Starting...");
+        });
+    }
+}
